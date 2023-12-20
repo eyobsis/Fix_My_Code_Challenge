f6ed81efbcd7cb1e5cb29bdf2714ad77c74d76eb
eyobsis <ehtube2020@gmail.com>
2023-12-20 00:26:13 +0000

    commit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
 
-class Square():
-    
-    width = 0
-        
-    for key, value in kwargs.items():
-            setattr(self, key, value)
+class Square:
+    """A class representing a square."""
 
-    def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+    def __init__(self, side_length):
+        """Initialize the Square object.
 
-    def PermiterOfMySquare(self):
-        return (self.width * 4)
+        Args:
+            side_length (int): The length of the square's sides.
+        """
+        self.side_length = side_length
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.side_length ** 2
+
+    def perimeter(self):
+        """Calculate the perimeter of the square.
+
+        Returns:
+            int: The perimeter of the square.
+        """
+        return 4 * self.side_length
 
     def __str__(self):
-        return "{}".format(self.width)
+        """Return a string representation of the square.
+
+        Returns:
+            str: The formatted string representation of the square.
+        """
+        return f"Square(side_length={self.side_length})"
 
 if __name__ == "__main__":
-
-    s = Square(width=12)
+    s = Square(side_length=12)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.perimeter())
